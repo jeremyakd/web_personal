4194e9c79dd0e1e355df79083a04a828b8303b72
jeremyakd <jeremiasc@exo.com.ar>
2019-11-21 14:40:25 -0300

    21-11

--- web_personal/portfolio/admin.py
+++ web_personal/portfolio/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Project
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+admin.site.register(Project,ProjectAdmin)
+
+
+
--- web_personal/portfolio/views.py
+++ web_personal/portfolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Project
 
 def portfolio(request):
-    return render(request, "portfolio/portfolio.html")
+    projects = Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {'projects':projects})
